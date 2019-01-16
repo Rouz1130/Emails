6306829aaccbc6a59d38b337ec90b3480a8a97de
Beck Johnson <beckjohnson@gmail.com>
2019-01-16 14:46:53 -0800

    add loop for report services

--- ReplaceEmailText.ps1
+++ ReplaceEmailText.ps1
@@ -1,12 +1,36 @@
-# Invoke-Command -computername $computerName {Remove-Item -path $args[0] -Recurse} -ArgumentList $RegKey
+
 $template = 'office365.html'
-$final_email =  'final_email.html'
+$final_email = 'final_email.html'
 
-(Get-Content $template) |
+$ReportServices =@(
 
-Foreach-Object {
-  $_ -replace '\[HEADER_COLOR\]','#0d1121'`
-     -replace '\[STATUS_MESSAGE\]','Investigating'
-  
-}  
-Set-Content $final_email
+"Microsoft Intune",
+"Dynamics 365",
+"Office 365 Portal",
+"Office Subscription",
+"Social Engagement",
+"Power BI",
+"Microsoft Teams",
+"Yammer Enterprise",
+"Yammer.com")
+
+$ReportServices =
+@([pscustomobject]@{name="Exchange Online";color="#D73b02", footer=""},
+[pscustomobject]@{name="SharePoint Online";color="#fff"},
+[pscustomobject]@{name="Skype for Business";color="#fff"})
+
+foreach ($service in $ReportServices)
+{
+
+  (Get-Content $template) |
+
+
+    Foreach-Object {
+      $_ -replace '\[HEADER_COLOR\]',$service.color`
+        -replace '\[REPORT_NAME\]',$service.name`
+        -replace '\[STATUS_MESSAGE\]','Investigating'
+      
+    }  |
+    Set-Content ($service.name + ".html")
+
+}
